8d36e6c75239202e7082c04613b8605d688c1c16
VenkatReddyv <93113837+VENKATREDDYVELAMURI@users.noreply.github.com>
2022-06-06 04:36:22 +0000

    completed

--- ActivitySet01/problem02.py
+++ ActivitySet01/problem02.py
@@ -1,3 +1,21 @@
-# Why Program
-
-prinq("hello world")
+str1="hello"
+str2='there'
+bob=str1+str2
+print(bob)
+str3="123"
+bob=int(str3)+1
+print(bob)
+print(int)
+str4='jat'''
+str="banana"
+x=3
+print(str[x])
+print(str[x-1])
+print(len(str))
+...
+name='reva'
+i=0
+while i<len(name):
+ letter = name[i]
+ print(i,letter)
+ i=i+1
